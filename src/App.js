969219f4fe62fd83231e712e7d57d5ee2dd6ce2e
CosmoUrMom <rasheedabbasov@gmail.com>
2025-06-06 07:41:53 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Login from './components/login';
-import login from './components/login'
+import login from './components/login';
+import Home from './components/home';
+import {  BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div>
-      <h1>Welcome</h1>
-      <Login />
-    </div>
+    <Router>
+      <div>
+        <h1>Welcome</h1>
+        <Login />
+        <Home />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { supabase } from '../supabaseClient';
+
+const Home = () => {
+
+
+
+    return (
+        <div>
+            Welcome to your tracker app!
+        </div>
+        
+    );
+
+
+
+
+    
+}
+
+export default Home;
