9152a7d97abc11b9419146e42969b63685fddb67
CosmoUrMom <rasheedabbasov@gmail.com>
2025-06-07 14:12:32 -0400

    REMOVED HARDCODED KEYS

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://onhfkrknxkugyxfldqdx.supabase.co'; // ✅ REAL URL from Supabase
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uaGZrcmtueGt1Z3l4ZmxkcWR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg3ODcwMjEsImV4cCI6MjA2NDM2MzAyMX0.XIaE_NUDWwLH_APKmVJJIlUOuPrLxiRQ2EAJrgzv3K4'; // ✅ REAL anon key from Supabase
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL; // ✅ REAL URL from Supabase
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY; // ✅ REAL anon key from Supabase
+
+// console.log("Supabase ULR:", process.env.REACT_APP_SUPABASE_URL);
+// console.log("Supabase Key:", process.env.REACT_APP_SUPABASE_ANON_KEY);
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
