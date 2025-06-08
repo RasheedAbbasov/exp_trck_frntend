f1b3db550759ec49120d4a9fc24bb4030acc7e97
CosmoUrMom <rasheedabbasov@gmail.com>
2025-06-08 12:09:54 -0400

    ADDED LOGIN CHECK

--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { supabase } from '../supabaseClient';
 import { useNavigate } from 'react-router-dom';
+
+
+// const signInWithGoogle = async () => {
+//     const { error } = await supabase.auth.signInWithOAuth({
+//       provider: 'google',
+//     });
+//     if (error) console.error('Login error:', error.message);
+//   };
+
+
+
+//   useEffect(() => {
+//     const {
+//       data: { subscription },
+//     } = supabase.auth.onAuthStateChange((event, session) =>   {
+//       if (session && session.user) {
+//         navigate('/expenses')
+//       }
+//     })
+//     return () => subscription.unsubscribe();
+//   }, [navigate]);
+
+
+
+
+const Login = () => {
+
+const navigate = useNavigate();
 
 
 const signInWithGoogle = async () => {
@@ -12,19 +40,22 @@ const signInWithGoogle = async () => {
 
 
 
+  useEffect(() => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((event, session) =>   {
+      if (session && session.user) {
+        navigate('/expenses')
+      }
+    })
+    return () => subscription.unsubscribe();
+  }, [navigate]);
 
 
-const Login = () => {
-  // const signInWithGoogle = async () => {
-  //   const { error } = await supabase.auth.signInWithOAuth({
-  //     provider: 'google',
-  //   });
-  //   if (error) console.error('Login error:', error.message);
-  // };
-  const navigate = useNavigate();
 
 
   return (
+    
     <div>
       <button onClick={signInWithGoogle}>Sign in with Google</button>
       <button>Sign in with Facebook</button>
